7d0bf6ff7226c0c46289b70e878e9b3e41d03cf7
Rasmus Bondesson <raek@raek.se>
2021-03-24 21:48:37 +0100

    Implement datetime_to_unix_time

--- firmware/datetime.c
+++ firmware/datetime.c
@@ -1,6 +1,18 @@
 #include "datetime.h"
 
+// Based on the beautifylly described algorithm by Howard Hinnant.
+// https://howardhinnant.github.io/date_algorithms.html
+
 UnixTime datetime_to_unix_time(uint16_t year, uint8_t month, uint8_t day, uint8_t hour, uint8_t minute, uint8_t second)
 {
-    return 0;
+    uint32_t myear = month <= 2 ? year - 1 : year;
+    uint32_t era = myear / 400;
+    uint32_t year_of_era = myear % 400;
+    uint32_t day_of_year = (153*(month + (month > 2 ? -3 : 9)) + 2)/5 + day - 1;
+    uint32_t day_of_era = (year_of_era*365) + (year_of_era/4) - (year_of_era/100) + day_of_year;
+    uint32_t days_since_epoch = (era*146097) + day_of_era - 719468;
+    uint32_t hours_since_epoch = (days_since_epoch*24) + hour;
+    uint32_t minutes_since_epoch = (hours_since_epoch*60) + minute;
+    uint32_t seconds_since_epoch = (minutes_since_epoch*60) + second;
+    return seconds_since_epoch;
 }
--- firmware/datetime_test.py
+++ firmware/datetime_test.py
@@ -1,4 +1,7 @@
 from firmware import datetime_to_unix_time
 
-def test_datetime_epoch():
+def test_epoch():
     assert datetime_to_unix_time(1970, 1, 1, 0, 0, 0) == 0
+
+def test_arbitrary_valid():
+    assert datetime_to_unix_time(2009, 2, 13, 23, 31, 30) == 1234567890
