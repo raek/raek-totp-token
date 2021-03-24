ae555d77fccb1b03a3b548ff048d0b950240a40a
Rasmus Bondesson <raek@raek.se>
2021-03-24 22:10:28 +0100

    Test all allowed unix times

--- firmware/check.sh
+++ firmware/check.sh
@@ -10,5 +10,8 @@ source .venv/bin/activate
 if [[ -z $(pip list | grep "^pytest") ]]; then
     pip install pytest
 fi
+if [[ -z $(pip list | grep "^hypothesis") ]]; then
+    pip install hypothesis
+fi
 python setup.py build install
 pytest
--- firmware/datetime_test.py
+++ firmware/datetime_test.py
@@ -1,7 +1,23 @@
+from datetime import datetime, timezone
+
+from hypothesis import given
+from hypothesis.strategies import integers
+
 from firmware import datetime_to_unix_time
+
+
+valid_unix_times = integers(min_value=0, max_value=(2^32-1))
+
 
 def test_epoch():
     assert datetime_to_unix_time(1970, 1, 1, 0, 0, 0) == 0
 
-def test_arbitrary_valid():
+
+def test_some_valid():
     assert datetime_to_unix_time(2009, 2, 13, 23, 31, 30) == 1234567890
+
+
+@given(valid_unix_times)
+def test_any_valid(unix_time):
+    dt = datetime.fromtimestamp(unix_time, tz=timezone.utc)
+    assert datetime_to_unix_time(*dt.timetuple()[0:6]) == unix_time
