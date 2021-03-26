48d19f1430e5d5d95540473157e98be9a1188c04
Rasmus Bondesson <raek@raek.se>
2021-03-26 19:07:31 +0100

    Switch datetime_py errors from bool-and-value to value-or-None

--- firmware/datetime_py.c
+++ firmware/datetime_py.c
@@ -14,5 +14,9 @@ PyObject *datetime_to_unix_time_py(PyObject *self, PyObject *args)
     }
     UnixTime result = 0;
     bool ok = datetime_to_unix_time(&result, year, month, day, hour, minute, second);
-    return Py_BuildValue("Ol", ok ? Py_True : Py_False, (long) result);
+    if (ok) {
+        return PyLong_FromLong(result);
+    } else {
+        Py_RETURN_NONE;
+    }
 }
--- firmware/datetime_test.py
+++ firmware/datetime_test.py
@@ -10,18 +10,18 @@ valid_unix_times = integers(min_value=0, max_value=(2**32-1))
 
 
 def test_epoch():
-    assert datetime_to_unix_time(1970, 1, 1, 0, 0, 0) == (True, 0)
+    assert datetime_to_unix_time(1970, 1, 1, 0, 0, 0) == 0
 
 
 def test_some_valid():
-    assert datetime_to_unix_time(2009, 2, 13, 23, 31, 30) == (True, 1234567890)
+    assert datetime_to_unix_time(2009, 2, 13, 23, 31, 30) == 1234567890
 
 
 def test_last_valid():
-    assert datetime_to_unix_time(2106, 2, 7, 6, 28, 15) == (True, 2**32 - 1)
+    assert datetime_to_unix_time(2106, 2, 7, 6, 28, 15) == 2**32 - 1
 
 
 @given(valid_unix_times)
 def test_any_valid(unix_time):
     dt = datetime.fromtimestamp(unix_time, tz=timezone.utc)
-    assert datetime_to_unix_time(*dt.timetuple()[0:6]) == (True, unix_time)
+    assert datetime_to_unix_time(*dt.timetuple()[0:6]) == unix_time
