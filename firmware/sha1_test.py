a2e248f8b54edd2536e4ae93d192716023ec4b50
Rasmus Bondesson <raek@raek.se>
2021-05-31 07:18:41 +0200

    Improve sha1 tests

--- firmware/sha1_test.py
+++ firmware/sha1_test.py
@@ -6,11 +6,24 @@ from hypothesis.strategies import binary, lists
 from firmware import Sha1
 
 
-@given(lists(binary()))
-def test_sha1(chunks: list[bytes]) -> None:
-    expected = sha1_oracle()
+def test_empty() -> None:
+    assert Sha1().digest().hex() == sha1_oracle().digest().hex()
+
+
+def test_short_example() -> None:
+    s = b"abc"
     actual = Sha1()
+    expected = sha1_oracle()
+    actual.update(s)
+    expected.update(s)
+    assert actual.digest().hex() == expected.digest().hex()
+
+
+@given(lists(binary()))
+def test_comparing_to_python_version(chunks: list[bytes]) -> None:
+    actual = Sha1()
+    expected = sha1_oracle()
     for chunk in chunks:
-        expected.update(chunk)
         actual.update(chunk)
-    assert expected.digest() == actual.digest()
+        expected.update(chunk)
+    assert actual.digest().hex() == expected.digest().hex()
