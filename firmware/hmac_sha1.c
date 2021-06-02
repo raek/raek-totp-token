42f86b5007152684fbc4ad5996e767e2881fe9d5
Rasmus Bondesson <raek@raek.se>
2021-06-02 20:46:01 +0200

    Half the number of xor operations in hmac_sha1.c

--- firmware/hmac_sha1.c
+++ firmware/hmac_sha1.c
@@ -5,7 +5,7 @@
 #define HMAC_INNER_PAD_BYTE 0x36
 #define HMAC_OUTER_PAD_BYTE 0x5c
 
-static inline void sha1_update_xor(Sha1 *sha1, uint8_t *data, size_t data_length, uint8_t xor_value);
+static inline void xor_bytes(uint8_t *data, size_t data_length, uint8_t xor_value);
 
 void hmac_sha1(HmacSha1 *scratchpad, const uint8_t *key, size_t key_length, const uint8_t *message, size_t message_length, uint8_t *code_output)
 {
@@ -21,25 +21,20 @@ void hmac_sha1(HmacSha1 *scratchpad, const uint8_t *key, size_t key_length, cons
     }
     // inner_digest = H((K0 (+) ipad) || message)
     sha1_init(&scratchpad->sha1);
-    sha1_update_xor(&scratchpad->sha1, scratchpad->key_block, SHA1_BLOCK_BYTES, HMAC_INNER_PAD_BYTE);
+    xor_bytes(scratchpad->key_block, SHA1_BLOCK_BYTES, HMAC_INNER_PAD_BYTE);
+    sha1_update(&scratchpad->sha1, scratchpad->key_block, SHA1_BLOCK_BYTES);
     sha1_update(&scratchpad->sha1, message, message_length);
     sha1_digest(&scratchpad->sha1, scratchpad->inner_digest);
     // code = H((K0 (+) opad) || inner_digest)
     sha1_init(&scratchpad->sha1);
-    sha1_update_xor(&scratchpad->sha1, scratchpad->key_block, SHA1_BLOCK_BYTES, HMAC_OUTER_PAD_BYTE);
+    xor_bytes(scratchpad->key_block, SHA1_BLOCK_BYTES, HMAC_INNER_PAD_BYTE ^ HMAC_OUTER_PAD_BYTE);
+    sha1_update(&scratchpad->sha1, scratchpad->key_block, SHA1_BLOCK_BYTES);
     sha1_update(&scratchpad->sha1, scratchpad->inner_digest, SHA1_DIGEST_BYTES);
     sha1_digest(&scratchpad->sha1, code_output);
 }
 
-static inline void sha1_update_xor(Sha1 *sha1, uint8_t *data, size_t length, uint8_t xor_value)
+static inline void xor_bytes(uint8_t *data, size_t length, uint8_t xor_value)
 {
-    // Modify data in-place to hold the xored version
-    for (size_t i = 0; i < length; i++) {
-        data[i] ^= xor_value;
-    }
-    // Update sha1 with xored version
-    sha1_update(sha1, data, length);
-    // Restore data to original state
     for (size_t i = 0; i < length; i++) {
         data[i] ^= xor_value;
     }
