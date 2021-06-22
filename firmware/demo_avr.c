b8bbb9f1a8c1f3aa78951c768ff07c7e7fb10af3
Rasmus Bondesson <raek@raek.se>
2021-06-22 12:44:25 +0200

    Add PIN_AVR_MAKE() macro

--- firmware/demo_avr.c
+++ firmware/demo_avr.c
@@ -7,18 +7,8 @@ int main(int argc, char **argv)
 {
     (void) argc;
     (void) argv;
-    Pin in_pin = {
-        .pin_reg     = &PIND,
-        .port_reg    = &PORTD,
-        .ddr_reg     = &DDRD,
-        .bit_pattern = 1<<7,
-    };
-    Pin out_pin = {
-        .pin_reg     = &PINC,
-        .port_reg    = &PORTC,
-        .ddr_reg     = &DDRC,
-        .bit_pattern = 1<<7,
-    };
+    Pin in_pin = PIN_AVR_MAKE(D, 7);
+    Pin out_pin = PIN_AVR_MAKE(C, 7);
     Inverter inverter;
     inverter_init(&inverter, &in_pin, &out_pin);
     for (;;) {
--- firmware/pin_avr.h
+++ firmware/pin_avr.h
@@ -4,6 +4,14 @@
 
 #include "pin.h"
 
+#define PIN_AVR_MAKE(letter, bit)                 \
+    {                                             \
+        .pin_reg     = &PIN ## letter,            \
+        .port_reg    = &PORT ## letter,           \
+        .ddr_reg     = &DDR ## letter,            \
+        .bit_pattern = 1<<(bit),                  \
+    }
+
 struct Pin {
     volatile uint8_t *pin_reg;
     volatile uint8_t *port_reg;
