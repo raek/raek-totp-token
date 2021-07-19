8368afb6afe902afe4509b4a8c177762808b7aa2
Rasmus Bondesson <raek@raek.se>
2021-07-20 01:05:26 +0200

    Determine callbacks by text list file instead of hacky macros

--- firmware/gen_cffi.py
+++ firmware/gen_cffi.py
@@ -8,15 +8,31 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-m", "--module-name",  required=True)
 parser.add_argument("-f", "--header-file",  required=True)
 parser.add_argument("-i", "--preproc-file", required=True)
+parser.add_argument("-c", "--callbacks-file", required=True)
 parser.add_argument("-o", "--output-file",  required=True)
 args = parser.parse_args()
 
+with open(args.callbacks_file, "rt", encoding="utf-8") as f:
+    callbacks = set(line.strip() for line in f)
+callbacks_regex = "(" + "|".join(re.escape(c) for c in callbacks) + ")"
+
 with open(args.preproc_file, "rt", encoding="utf-8") as f:
     declarations = f.read()
-# Remove compiler-defined macros and the internal CFFI and API_FN macro
-declarations = re.sub(r"^#define (__.*__|CFFI|API_FN)$", "", declarations, flags=re.MULTILINE)
+# Remove compiler-defined macros
+declarations = re.sub(r"^#define __\w+__",
+                      "",
+                      declarations,
+                      flags=re.MULTILINE)
 # Make macro value always be "..."
-declarations = re.sub(r"^(#define \w+).*$", lambda m: m.group(1) + " ...", declarations, flags=re.MULTILINE)
+declarations = re.sub(r"^(#define \w+).*$",
+                      lambda m: m.group(1) + " ...",
+                      declarations,
+                      flags=re.MULTILINE)
+# Mark callbacks with special CFFI syntax
+declarations = re.sub(r"^(.*\b" + callbacks_regex + r"\b.*)$",
+                      lambda m: 'extern "Python+C" ' + m.group(1),
+                      declarations,
+                      flags=re.MULTILINE)
 
 builder = FFI()
 builder.cdef(declarations)
--- firmware/pin.h
+++ firmware/pin.h
@@ -2,19 +2,12 @@
 
 #include <stdbool.h>
 
-// TODO: avoid having this header file know about this
-#ifdef CFFI
-#define API_FN extern "Python+C"
-#else
-#define API_FN
-#endif
-
 #define PIN_DIR_INPUT false
 #define PIN_DIR_OUTPUT true
 
 struct Pin;
 typedef struct Pin Pin;
 
-API_FN void pin_set_dir(Pin *pin, bool output);
-API_FN void pin_write(Pin *pin, bool state);
-API_FN bool pin_read(Pin *pin);
+void pin_set_dir(Pin *pin, bool output);
+void pin_write(Pin *pin, bool state);
+bool pin_read(Pin *pin);
