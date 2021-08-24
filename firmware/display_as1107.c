eb29f0a9b917d78dd363f01d27bc9bb56d1a269a
Rasmus Bondesson <raek@raek.se>
2021-08-24 22:17:41 +0200

    Print display contents to debug

--- firmware/display_as1107.c
+++ firmware/display_as1107.c
@@ -1,4 +1,5 @@
 #include "as1107.h"
+#include "debug.h"
 #include "font.h"
 #include "display.h"
 #include "display_as1107.h"
@@ -15,16 +16,26 @@ void display_set_intensity(struct display *display, uint8_t intensity)
 
 void display_show_text(struct display *display, const char *text)
 {
+    debug_put_string("display: [");
+    for (int i = 0; i < DISPLAY_CHAR_COUNT; i++) {
+        if (text[i] == '\0') {
+            break;
+        }
+        debug_put_char(text[i]);
+    }
+    debug_put_string("]\r\n");
     font_render(display->glyphs_scratchpad, AS1107_GLYPH_COUNT, text);
     as1107_show_glyphs(display->as1107, display->glyphs_scratchpad);
 }
 
 void display_blank(struct display *display)
 {
+    debug_put_string("display: blank\r\n");
     as1107_blank(display->as1107);
 }
 
 void display_resume(struct display *display)
 {
+    debug_put_string("display: resume\r\n");
     as1107_resume(display->as1107);
 }
